2daa1722f4cc88513cc32f87d843b97689f9a3d5
HankMadden <46724260+HankMadden@users.noreply.github.com>
2019-01-23 16:49:29 -0500

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,33 @@
+
+/************************
+ * Project 1- Name Art
+ *Project outputs my name in a unique way, here based on my birthday 11/15/1995
+ *_______________________
+ * Hank Madden
+ * 1/23/2019
+ * CMSC 255-002
+ ************************/
+ public class NameArt {
+   public static void printHeading() {
+     System.out.println("Hank Madden");
+	 System.out.println("Project 1-Name in Code");
+	 System.out.println("This project outputs my name in a unique way on the display screen based on my birthday 11/15/1995");
+	 System.out.println("1/23/2019");
+	 System.out.println("CSMC255-002");
+	 System.out.println();
+}
+
+public static void main(String [] args) {
+   printHeading();
+   System.out.println(" 11       11              1995              //  15                 15          15         15");
+   System.out.println(" 11       11            11    11           //   15 15              15          15       15");
+   System.out.println(" 11       11           11      11         //    15   15            15          15     15");
+   System.out.println(" 11       11          11        11       //     15     15          15          15   15");
+   System.out.println(" 11111111111          111111111111      //      15       15        15          15 15 ");
+   System.out.println(" 11       11          11        11     //       15         15      15          15 15  ");
+   System.out.println(" 11       11          11        11    //        15           15    15          15   15");
+   System.out.println(" 11       11          11        11   //         15             15  15          15     15");
+   System.out.println(" 11       11          11        11  //          15               1515          15       15");
+   System.out.println(" 11       11          11        11 //           15                 15          15         15");
+   } 
+} 
